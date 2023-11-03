7048c8a82c487c240fe193e662d0a3a8424f716c
Iaroslav Kovalchuk <iaroslavkovalchuk@gmail.com>
2023-11-03 05:30:14 +0200

    sl

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 
+class Dense_v1(nn.Module):
+    def __init__(self):
+        super(Dense_v1, self).__init__()
+
+        self.hidden_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(240, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
+        )
+
+    def forward(self, x):
+        return self.hidden_layer(x)
+
+
 class Dense_v2(nn.Module):
     def __init__(self):
         super(Dense_v2, self).__init__()
@@ -58,6 +77,10 @@ minmax_model = Dense_v2()
 minmax_model.load_state_dict(torch.load('models/minmax.pth', map_location=device))
 minmax_model.eval()
 
+sl_model = Dense_v1()
+sl_model.load_state_dict(torch.load('models/sl_model.pth', map_location=device))
+sl_model.eval()
+
 
 app = FastAPI()
 app.add_middleware(
@@ -96,6 +119,15 @@ async def root(item: ModelItem):
             xx = (x - x[0, -1, 3]) / d
             y = (minmax_model(xx)[0] * d).detach().cpu().numpy()
             return "%f,%f" % (max(y[0], 0) + data[-1], min(y[1], 0) + data[-1])
+
+        elif item.name == 'sl':
+            data = item.data
+            x = torch.tensor([data[:240]], dtype=torch.float32).view(1, 4, 60).transpose(1, 2).to(device)
+            d = (x.max() - x.min()) / 5
+            xx = (x - x[0, -1, 3]) / d
+            y = (sl_model(xx)[0] * d).detach().cpu().numpy()
+            return "%f,%f" % (max(y[0], 0) + data[-1], min(y[1], 0) + data[-1])
+        
     except:
         return "Failed"
 
